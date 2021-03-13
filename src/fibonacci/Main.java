e8261a9e40edf6b9c789b7e6b11c94318cf79705
mattia <geom.dibattistamattia@gmail.com>
2021-03-13 08:26:16 +0100

    Better view of output

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -5,14 +5,12 @@ public class Main {
 	//F(n) = F(n-1) + F(n-2) 
     public static void main(String[] args) {
 
-        int n = 510, n_1 = 0, n_2 = 1, sum = 0;
+        int n = 10, n_1 = 0, n_2 = 1, sum = 0;
         System.out.print("First " + n + " terms: ");
 
         for (int i = 1; i <= n; ++i){
         	
-        	System.out.print(sum);
-        	if (i < n){System.out.print(" + ");}
-        	
+        	System.out.print(sum + "\n");        	
             sum = n_1 + n_2;
             n_1 = n_2;
             n_2 = sum;
