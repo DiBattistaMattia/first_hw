d7c3e036a3db5dee3baea7fd65efb133e757ed1a
mattia <geom.dibattistamattia@gmail.com>
2021-03-13 08:30:19 +0100

    Added comments

--- src/fibonacci/Main.java
+++ src/fibonacci/Main.java
@@ -1,12 +1,18 @@
 package fibonacci;
 
+/*
+ * the main class calculate the fibonacci's
+ * series of number n
+ * F(n) = F(n-1) + F(n-2) 
+ *
+ */
+
 public class Main {
 
-	//F(n) = F(n-1) + F(n-2) 
     public static void main(String[] args) {
 
         int n = 10, n_1 = 0, n_2 = 1, sum = 0;
-        System.out.print("First " + n + " terms: ");
+        System.out.print("First " + n + " terms: \n");
 
         for (int i = 1; i <= n; ++i){
         	
